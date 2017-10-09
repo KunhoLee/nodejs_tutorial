e7200e308cd2c40c29026fd3f29bd490b5cec401
KunhoLee <gunho1020@gmail.com>
2017-10-09 21:38:16 +0900

    nodejs tutorial
    10-1_EXPRESS_EJS
    
    Reference: https://velopert.com/379

--- express_tutorial/server.js
+++ express_tutorial/server.js
@@ -1,12 +1,23 @@
 var express = require('express'),
+    bodyParser = require('body-parser'),
+    session = require('express-session'),
+    fs = require('fs'),
     app = express();
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+
 app.engine('html', require('ejs').renderFile);
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(session({
+    'secret': 'alfhl%##@%^FSddd',
+    'resave': false,
+    'saveUninitialized': true
+}));
 
-require('./router/main')(app);
+require('./router/main')(app, fs);
 
 app.listen(3000, () => console.log('Express server has started on port 3000'));
