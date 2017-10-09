cedd857a6350faf146d4561ab40c214fc2fa41c1
KunhoLee <gunho1020@gmail.com>
2017-10-09 22:38:34 +0900

    nodejs tutorial
    10-2_EXPRESS_RESTFUL_API
    
    Reference: https://velopert.com/332

--- express_tutorial/router/main.js
+++ express_tutorial/router/main.js
@@ -3,4 +3,113 @@ module.exports = (app, fs) => {
         'title': 'MY HOMEPAGE',
         'length': 5
     }));
+    app.get('/list', (req, res) => fs.readFile(
+        __dirname + '/../data/' + 'user.json',
+        'utf8',
+        (err, data) => {
+            console.log(data);
+            res.end(data);
+        }
+    ));
+    app.get('/getUser/:username', (req, res) => fs.readFile(
+        __dirname + '/../data/' + 'user.json',
+        'utf8',
+        (err, data) => {
+            var users = JSON.parse(data);
+            res.json(users[req.params.username]);
+        }
+    ));
+    app.post('/addUser/:username', (req, res) => {
+        var result = {},
+            username = req.params.username;
+        // CHECK REQ VALIDITY
+        if (!req.body['password'] || !req.body['name']) {
+            result['success'] = 0;
+            result['error'] = 'invalid request';
+            return res.json(result);
+        }
+        // LOAD DATA & CHECK DUPLICATION
+        fs.readFile(
+            __dirname + '/../data/' + 'user.json',
+            'utf8',
+            (err, data) => {
+                var users = JSON.parse(data);
+                if (users[username]) {
+                    // DUPLICATION FOUND
+                    result['success'] = 0;
+                    result['errer'] = 'duplicate';
+                    return res.json(result);
+                }
+                // ADD TO DATA
+                users[username] = req.body;
+                // SAVE DATA
+                fs.writeFile(
+                    __dirname + '/../data/' + 'user.json',
+                    JSON.stringify(users, null, '\t'),
+                    'utf8',
+                    (err, data) => {
+                        result['success'] = 1;
+                        res.json(result);
+                    }
+                );
+            }
+        );
+    });
+    app.put('/updateUser/:username', (req, res) => {
+        var result = {},
+            username = req.params.username;
+        // CHECK REQ VALIDITY
+        if (!req.body['password'] || !req.body['name']) {
+            result['success'] = 0;
+            result['error'] = 'invalid request';
+            return res.json(result);
+        }
+        // LOAD DATA
+        fs.readFile(
+            __dirname + '/../data/' + 'user.json',
+            'utf8',
+            (err, data) => {
+                var users = JSON.parse(data);
+                // ADD/MODIFY DATA
+                users[username] = req.body;
+                // SAVE DATA
+                fs.writeFile(
+                    __dirname + '/../data/' + 'user.json',
+                    JSON.stringify(users, null, '\t'),
+                    'utf8',
+                    (err, data) => {
+                        result['success'] = 1;
+                        res.json(result);
+                    }
+                );
+            }
+        );
+    });
+    app.delete('/deleteUser/:username', (req, res) => {
+        var result = {};
+        // LOAD DATA
+        fs.readFile(
+            __dirname + '/../data/' + 'user.json',
+            'utf8',
+            (err, data) => {
+                var users = JSON.parse(data);
+                // IF NOT FOUND
+                if (!users[req.params.username]) {
+                    result['success'] = 0;
+                    result['error'] = 'not found';
+                    return res.json(result);
+                }
+                delete users[req.params.username];
+                fs.writeFile(
+                    __dirname + '/../data/' + 'user.json',
+                    JSON.stringify(users, null, '\t'),
+                    'utf8',
+                    (err, data) => {
+                        result['success'] = 1;
+                        return res.json(result);
+                    }
+                );
+            }
+        );
+    });
 };
